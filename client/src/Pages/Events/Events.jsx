6bb3e0e1887872f62e8d1f3c4d680571b6db9dcd
Kalash Jain <kalashjain446@gmail.com>
2023-02-16 00:12:08 +0530

    initial commit

--- client/src/Pages/Events/Events.jsx
+++ client/src/Pages/Events/Events.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
-
+import './Events.css'
 export default function Events() {
   return (
-    <div>Events</div>
+    
+    <div className='container effect'>
+      <div className='d-flex justify-content-center align-items-center'>
+      <h1 className='text-center'>Upcoming Events</h1></div>
+      <div className=''>
+      <h1 className='text-center'>Past Events</h1>
+      </div>
+
+    </div>
+    
   )
 }
