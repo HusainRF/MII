136921ea44b16625cbfb68041947f6c3d470c9c5
sharma39vishal <sharma39vishal@gmail.com>
2023-02-15 12:12:15 +0530

    Routes added

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Home from './Pages/Home/Home.jsx'
+import Blogs from './Pages/Blog/Blogs.jsx'
+import Events from './Pages/Events/Events.jsx'
+import Contactus from './Pages/Contactus/Contactus.jsx'
+import Ourofferings from './Pages/Our-Offerings/OurOfferings.jsx'
+import { Route,Routes } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route exact path='/' element={<Home/> }/>
+        <Route  path='/ourofferings' element={<Ourofferings/> }/>
+        <Route  path='/blogs' element={<Blogs/> }/>
+        <Route  path='/events' element={<Events/> }/>
+        <Route  path='/contactus' element={<Contactus/> }/>
+      </Routes>
     </div>
   );
 }
