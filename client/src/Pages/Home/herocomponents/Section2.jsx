4f57dd61382a9ac95b6c42d9b1f568f3781dee41
jain-dishika <87193284+jain-dishika@users.noreply.github.com>
2023-02-16 08:35:49 +0530

    ui updated

--- client/src/Pages/Home/herocomponents/Section2.jsx
+++ client/src/Pages/Home/herocomponents/Section2.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import './Section2.css'
+import React from "react";
+import "./Section2.css";
 export default function Section2() {
   return (
-    <div className='section2'>
-    <h1>Identity and Culture </h1>
-      <h4>Celebrating diversity and embracing new ideas, MII Foundation's culture is centered around innovation and creativity. At MII Foundation, we have a culture that is collaborative, inclusive, and centered around innovation and creativity. We believe in fostering a supportive environment where start-ups can come together to share ideas, knowledge, and experiences. </h4>
+    <div className="section2">
+      <h1>Identity and Culture </h1>
+      <p>
+        Celebrating diversity and embracing new ideas, MII Foundation's culture
+        is centered around innovation and creativity. At MII Foundation, we have
+        a culture that is collaborative, inclusive, and centered around
+        innovation and creativity. We believe in fostering a supportive
+        environment where start-ups can come together to share ideas, knowledge,
+        and experiences. 
+      </p>
     </div>
-  )
+  );
 }
--- client/src/Universal/Footer/Footer.jsx
+++ client/src/Universal/Footer/Footer.jsx
@@ -1,32 +1,35 @@
-import React from 'react'
-import Copyright from './Copyright'
-import './footer.css'
+import React from "react";
+import Copyright from "./Copyright";
+import "./footer.css";
 export default function Footer() {
   return (
-    <div className='footer-box'>
+    <div className="footer-box">
+      <div className="footer-container">
         <h1>Want to Know More?</h1>
-        <u style={{color:"green"}}><h1>Get in Touch</h1></u>
-
-        <div className='footer-bottom'>
-            <div className='bootom-firstline'>
-              <div>
-            <img src='./images/medicaps.png' alt='' width={"120px"}/>
-            <img src='./images/25year.png' alt='' width={"100px"}/>
+        <u>
+          <h1>Get in Touch</h1>
+        </u>
+        <div className="footer-bottom">
+          <div className="bootom-firstline">
+            <div>
+              <img src="./images/medicaps.png" alt="" width={"120px"} />
+              <img src="./images/25year.png" alt="" width={"100px"} />
             </div>
-            <div className='footer-pages'>
-            <h6>Home</h6>
-            <h6>Our Offerings</h6>
-            <h6>Blogs</h6>
-            <h6>Events</h6>
+            <div className="footer-pages">
+              <h6>Home</h6>
+              <h6>Our Offerings</h6>
+              <h6>Blogs</h6>
+              <h6>Events</h6>
             </div>
-            </div>
-            <div className='social-icons'>
+          </div>
+          <div className="social-icons">
             <i class="fa-brands fa-facebook"></i>
             <i class="fa-brands fa-instagram"></i>
             <i class="fa-brands fa-twitter"></i>
-            </div>
+          </div>
         </div>
-        <Copyright/>
+      </div>
+      <Copyright />
     </div>
-  )
+  );
 }
