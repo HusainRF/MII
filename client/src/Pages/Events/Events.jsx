da4ffa023cb64cc4d20c5c8f99e94609674e093b
Kalash Jain <kalashjain446@gmail.com>
2023-02-16 19:58:02 +0530

    temp

--- client/src/Pages/Events/Events.jsx
+++ client/src/Pages/Events/Events.jsx
@@ -3,11 +3,14 @@ import './Events.css'
 export default function Events() {
   return (
     
-    <div className='container effect'>
-      <div className='d-flex justify-content-center align-items-center'>
-      <h1 className='text-center effect'>Upcoming Events</h1></div>
+    <div className='container '>
+      <div className=''>
+      <h1 className='text-center effect'>Upcoming Events</h1>
+      <p className='text-danger slide-fwd-top '>hello world</p>
+      </div>
       <div className=''>
       <h1 className='text-center effect'>Past Events</h1>
+      
       </div>
 
     </div>
