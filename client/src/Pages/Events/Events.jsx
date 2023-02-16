f6fcc52e6eb8f0b63dcff74afb4c5bbbb838af3c
Kalash Jain <kalashjain446@gmail.com>
2023-02-16 22:15:48 +0530

    event updated

--- client/src/Pages/Events/Events.jsx
+++ client/src/Pages/Events/Events.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import './Events.css'
+import './EventComponent'
+import EventComponent from './EventComponent'
 export default function Events() {
   return (
     
     <div className='container '>
       <div className=''>
       <h1 className='text-center effect'>Upcoming Events</h1>
-      <p className='text-danger slide-fwd-top '>hello world</p>
+      <div className='d-flex row justify-content-center'>
+        
+<EventComponent/>
+{/* <EventComponent/> */}
+{/* <EventComponent/> */}
+
+
+        
+
+      </div>
+      {/* <p className='text-danger slide-fwd-top '>hello world</p> */}
       </div>
       <div className=''>
       <h1 className='text-center effect'>Past Events</h1>
