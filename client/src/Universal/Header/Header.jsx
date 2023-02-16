6b5f9753a534d9aabcd02bcbcb8e9a7b68665113
sharma39vishal <sharma39vishal@gmail.com>
2023-02-16 20:36:41 +0530

    header

--- client/src/Universal/Header/Header.jsx
+++ client/src/Universal/Header/Header.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import "./header.css";
 export default function Header() {
   return (
-    <div className="nav-header">
-      <div className="header-bar">
+    <div className="nav-header zl-navbar-strip">
+      <div className="zl-navbar-container header-bar">
         <div className="header-img">
-          <img src="./images/medicaps.png" alt="" width={"80px"} />
+          <a href="/"><img src="./images/medicaps.png" alt="" width={"80px"} /></a>
         </div>
         <div className="item1">
-          <a href="/">Home</a>
           <a href="/ourofferings">Our Offerings</a>
           <a href="/events">Events</a>
           <a href="/blogs">Blogs</a>
