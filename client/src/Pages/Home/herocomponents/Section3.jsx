cc5e936b62d8702b4d58dbb0bb1f257c2bc2903f
sharma39vishal <sharma39vishal@gmail.com>
2023-02-17 15:29:29 +0530

    deloy

--- client/src/Pages/Home/herocomponents/Section3.jsx
+++ client/src/Pages/Home/herocomponents/Section3.jsx
@@ -11,7 +11,7 @@ export default function Section3() {
         className="zl-feature-fill"
         style={{ backgroundColor: "rgba(0, 44, 248, 0.03)" }}
       />
-      <div className="feature-container">
+      {/* <div className="feature-container">
         <div className="feature-section">
           <div className="feature-contain blue">
             <h1>Collaborative</h1>
@@ -21,13 +21,13 @@ export default function Section3() {
           </div>
           <div className="feature-contain green">
             <h1>Innovation</h1>
-          </div>
+          </div> */}
           {/* <div className="feature-action">
             <a className="zl-btn zl-btn_cta" href="/aboutus">
               <span>About us</span>
             </a>
           </div> */}
-        </div>
+        {/* </div>
         <div className="feature-img">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/4/4d/Ruby-Diamond-88757.gif"
@@ -35,7 +35,7 @@ export default function Section3() {
             srcset=""
           />
         </div>
-      </div>
+      </div> */}
       {/* <div className="feature-section">
         <div className="feature-content">
           <div className="feature-container">
@@ -68,6 +68,38 @@ export default function Section3() {
           </div>
         </div>
       </div> */}
+      <div className="zl-feature-section -secondary" style={{}}>
+        <div className="zl-feature-content">
+          <div className="zl-feature-container">
+            <div className="zl-feature-grid">
+              <div className="zl-feature-grid-col -left">
+                <div className="zl-feature-header">
+                  <h2>Collaborative</h2>
+                </div>
+                <div className="zl-feature-description">
+                  {/* <p>Community, communication, collaboration</p> */}
+                </div>
+              </div>
+              <div className="zl-feature-grid-col -right">
+                <div className="zl-feature-img">
+                  <picture>
+                    <source
+                      srcSet="/assets/img/feature/2.png?1"
+                      media="(max-width:768px)"
+                    />
+                    <img
+                      src="/assets/img/feature/2.png?1"
+                      srcSet="/assets/img/feature/2@2x.png 2x"
+                      alt=""
+                      loading="eager"
+                    />
+                  </picture>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="zl-feature-section -tertiary" style={{}}>
         <div className="zl-feature-content">
           <div className="zl-feature-container">
