546a46822ab8648e0e76ad104dc582a3257ba55d
Kalash Jain <kalashjain446@gmail.com>
2023-03-07 14:39:40 +0530

    temp

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Herosection from './herocomponents/Herosection'
 import Section2 from './herocomponents/Section2'
 import Section3 from './herocomponents/Section3'
 import OurTeam from './herocomponents/OurTeam.jsx'
+import Sectionfinal from './herocomponents/sectionfinal'
 // import OurAdvisor from './OurAdvisor'
 import Advisors from './herocomponents/Advisors'
 import Testimonials from './Testimonials'
@@ -11,8 +12,9 @@ export default function Home() {
   return (
     <div>
       <Herosection/>
-      <Section2/>
-      <Section3/>
+      {/* <Section2/> */}
+      {/* <Section3/> */}
+      <Sectionfinal/>
       <OurTeam/>
       {/* <OurAdvisor/> */}
       
