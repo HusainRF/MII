04f5c1150f44d759605645cdad3c01db0a27cc93
Kalash Jain <kalashjain446@gmail.com>
2023-03-07 15:29:56 +0530

    updation

--- client/src/Pages/Home/herocomponents/sectionfinal.jsx
+++ client/src/Pages/Home/herocomponents/sectionfinal.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
-
+import { NavLink } from 'react-router-dom'
+import './SectionFinal.css'
 export default function Sectionfinal() {
     return (
         <div className='container-fluid'>
             <div className=' d-flex row text-center align-items-center  '>
-                <div className='col-md-8 col-10 d-flex row align-items-center' style={{height:"80vh"}}>
-                   
-                   <div style={{color:"blue"}}>
-                    <h1 style={{
-                        fontSize: "80px",
-                        // lineHeight: "80px",
-                        
-                    }} >Incubation</h1>
-                    <p style={{
-                        fontSize: "24px",
-                        lineHeight: "35px",
-                        opacity:".5"
-                    }}>Ideation, technology, compliance</p></div>
+                <div className='col-md-8 col-10 d-flex row align-items-center' style={{ height: "80vh" }}>
 
-                    
-                    
+                    <div style={{ color: "blue" }}>
+                        <h1 style={{
+                            fontSize: "80px",
+                            // lineHeight: "80px",
+
+                        }} >Incubation</h1>
+                        <p style={{
+                            fontSize: "24px",
+                            lineHeight: "35px",
+                            opacity: ".5"
+                        }}>Ideation, technology, compliance</p></div>
+
+
+
                 </div>
                 <div className='col-md-4 col -10'>
                     <img src='	https://www.zenxlabs.io/assets/img/feature/2.png?1' />
@@ -27,23 +28,23 @@ export default function Sectionfinal() {
 
             </div>
             <div className=' d-flex row text-center align-items-center  '>
-                <div className='col-md-8 col-10 d-flex row align-items-center' style={{height:"80vh"}}>
-                   
-                   <div style={{color:'purple'}}>
-                    <h1 style={{
-                        fontSize: "80px",
-                        color:"purple"
-                        // lineHeight: "80px",
-                        
-                    }} >Incubation</h1>
-                    <p style={{
-                        fontSize: "24px",
-                        lineHeight: "35px",
-                        opacity:".5"
-                    }}>Ideation, technology, compliance</p></div>
+                <div className='col-md-8 col-10 d-flex row align-items-center' style={{ height: "80vh" }}>
 
-                    
-                    
+                    <div style={{ color: 'purple' }}>
+                        <h1 style={{
+                            fontSize: "80px",
+                            color: "purple"
+                            // lineHeight: "80px",
+
+                        }} >Accelaration</h1>
+                        <p style={{
+                            fontSize: "24px",
+                            lineHeight: "35px",
+                            opacity: ".5"
+                        }}>Ideation, technology, compliance</p></div>
+
+
+
                 </div>
                 <div className='col-md-4 col -10'>
                     <img src='https://www.zenxlabs.io/assets/img/feature/1.png?1' />
@@ -51,29 +52,30 @@ export default function Sectionfinal() {
 
             </div>
             <div className=' d-flex row text-center align-items-center  '>
-                <div className='col-md-8 col-10 d-flex row align-items-center' style={{height:"80vh"}}>
-                   
-                   <div style={{    color:" #008a6e"}}>
-                    <h1 style={{
-                        fontSize: "80px",
-                        // lineHeight: "80px",
-                        
-                    }} >Incubation</h1>
-                    <p style={{
-                        fontSize: "24px",
-                        lineHeight: "35px",
-                        opacity:".5"
-                    }}>Ideation, technology, compliance</p></div>
+                <div className='col-md-8 col-10 d-flex row align-items-center' style={{ height: "80vh" }}>
 
-                    
-                    
+                    <div style={{ color: " #008a6e" }}>
+                        <h1 style={{
+                            fontSize: "80px",
+                            // lineHeight: "80px",
+
+                        }} >Adoption</h1>
+                        <p style={{
+                            fontSize: "24px",
+                            lineHeight: "35px",
+                            opacity: ".5"
+                        }}>Ideation, technology, compliance</p></div>
+                    <div >
+                        <NavLink to='/about' className='abc'><button  className='pqr'>About Us</button></NavLink>
+                    </div>
                 </div>
                 <div className='col-md-4 col -10'>
                     <img src='	https://www.zenxlabs.io/assets/img/feature/3.png?1' />
                 </div>
 
+
             </div>
-           
+
         </div >
     )
 }
