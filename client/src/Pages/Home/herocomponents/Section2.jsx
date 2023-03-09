079c8f1aeb2ad717e7e77ed3d1aae301ea70c734
HusainRF <justgetupanddoit9@gmail.com>
2023-03-09 23:18:07 +0530

    middle section completed home page

--- client/src/Pages/Home/herocomponents/Section2.jsx
+++ client/src/Pages/Home/herocomponents/Section2.jsx
@@ -9,16 +9,15 @@ export default function Section2() {
       </div>
       <div className="morein" >
          At MII Foundation, we have a culture that is collaborative, inclusive, and centered around innovation and creativity. <span className="greenColor"> We believe in fostering a supportive environment where start-ups can come together to share ideas, knowledge, and experiences.<span></span>
-</span>
+      </span>
       </div>
     </div>
-  
-      <div className="someth">
+
+      <div className="someth1">
         <div className="head">
-          <h3>Discover your passion and kickstart your business journey</h3>
+          Discover your passion and kickstart your business journey
         </div>
         <div className="content">
-          <p>
             At MII, we are dedicated to helping entrepreneurs unlock their full
             potential and achieve success. Our team of experienced professionals
             provide a range of services including co-working, mentorship,
@@ -27,12 +26,14 @@ export default function Section2() {
             trends, including articles and insights, events and webinars, and
             success stories. If you're ready to take your business to the next
             level, contact us today to get started.
-          </p>
+            <br/>
+            <br/>
           <button id="btn" style={{ display: "none" }}></button>
           <label for="btn" style={{ cursor: "pointer" }}>
-            Get more information about MII Foundation {"-->"}
+            <span className="underLi"> Get more information about MII Foundation {"-->"} </span><span/> 
           </label>
         </div>
+   
       </div>
     </>
   );
