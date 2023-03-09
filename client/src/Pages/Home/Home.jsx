607781c6e6c50d32e6a5674e3b1380eb7b87d361
unknown <parmarsinghbhanu@gmail.com>
2023-03-09 13:36:05 +0530

    updated

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -18,7 +18,7 @@ export default function Home() {
       <OurTeam/>
       {/* <OurAdvisor/> */}
       
-    <Advisors/>
+    <Advisors/>              
       <OurTeam/>
     {/* <Diamond/> */}
       <Testimonials/>
